b119b089e574328dc67ffd0c67ba19095b80c667
andrez <andrez@htmind.com.br>
2023-01-02 09:29:07 -0300

    Change port to 8880 - CloudFlare Proxy

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
 const app = require('./config/express')();
 const port = app.get('port');
-const https = require('https');
-const fs = require('fs')
 
-const optionsHttps = {
-    key: fs.readFileSync('./key.pem'),
-    cert: fs.readFileSync('./cert.pem')
-};
 
-/*app.listen(port, () =>{
+app.listen(port, () =>{
     console.log(`Servidor rodando na porta ${port}`);
-})*/
-
-https.createServer(optionsHttps, app).listen(port)
+})
