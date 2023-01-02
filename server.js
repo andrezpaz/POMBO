cbd0d2d791d9327929de305d8d16ab695a16dcf4
andrez <andrez@htmind.com.br>
2023-01-02 09:42:14 -0300

    Change port to 2096 - CloudFlare Proxy

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const app = require('./config/express')();
 const port = app.get('port');
+const https = require('https');
+const fs = require('fs')
 
+const optionsHttps = {
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem')
+};
 
-app.listen(port, () =>{
+/*app.listen(port, () =>{
     console.log(`Servidor rodando na porta ${port}`);
-})
+})*/
+
+https.createServer(optionsHttps, app).listen(port)
