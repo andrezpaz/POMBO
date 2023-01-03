bed447e658917e65013ca1d4b5760df58978c180
andrez <andrez@htmind.com.br>
2023-01-03 17:57:49 -0300

    Adicionado setor do funcionario

--- api/data/namesMetadados.js
+++ api/data/namesMetadados.js
@@ -8,11 +8,14 @@ module.exports =  app => {
                           rhcontratos.cargo idCargo, 
                           rhcargos.descricaooficial descricaoCargo,
                           rhcontratos.setor idSetor,
-                          rhsetores.descricao40 descricaoSetor
-                     FROM rhcontratos, rhpessoas, rhcargos, rhsetores
-                    WHERE rhcontratos.pessoa   = rhpessoas.pessoa
-                      AND rhcontratos.cargo    = rhcargos.cargo
-                      AND rhcontratos.setor    = rhsetores.setor
+                          rhsetores.descricao40 descricaoSetor,
+                          rhcentroscusto2.centrocusto2 idCentroCusto,
+                          rhcentroscusto2.descricao40 descricaoCentroCusto 
+                     FROM rhcontratos, rhpessoas, rhcargos, rhsetores, rhcentroscusto2
+                    WHERE rhcontratos.pessoa        = rhpessoas.pessoa
+                      AND rhcontratos.cargo         = rhcargos.cargo
+                      AND rhcontratos.setor         = rhsetores.setor
+                      AND rhcontratos.centrocusto2  = rhcentroscusto2.centrocusto2
                       AND rhcontratos.situacao in (1,2)`
        
         let data =  connectionOracle.queryMetadados(sql)
